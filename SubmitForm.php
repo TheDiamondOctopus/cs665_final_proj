6e7ae5d5abf1d4427e7f9830ab777fdcd46b3e19
jcamp649 <jcamp649@bu.edu>
2018-11-26 15:13:31 -0500

    changed rand range

--- SubmitForm.php
+++ SubmitForm.php
@@ -28,7 +28,7 @@
             (?, ?, ?, ?, ?, ?)";  
 
     /* Set parameter values. */  
-    $params = array(rand(3,1000), $FirstName, $LastName, $EmailAddress, $Department, $StartDate);  
+    $params = array(rand(100000,999999), $FirstName, $LastName, $EmailAddress, $Department, $StartDate);  
 
     /* Prepare and execute the query. */  
     $stmt = sqlsrv_query($conn, $tsql, $params);  
